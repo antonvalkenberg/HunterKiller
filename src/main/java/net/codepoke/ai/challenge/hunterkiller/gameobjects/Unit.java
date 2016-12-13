90fdb8539668ae64876acc5cdc58fb652d9c8b79
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2016-12-13 17:59:00 +0100

    [Anton] Unit outline.

--- src/main/java/net/codepoke/ai/challenge/hunterkiller/gameobjects/Unit.java
+++ src/main/java/net/codepoke/ai/challenge/hunterkiller/gameobjects/Unit.java
@@ -1,30 +1,159 @@
 package main.java.net.codepoke.ai.challenge.hunterkiller.gameobjects;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import main.java.net.codepoke.ai.challenge.hunterkiller.enums.Direction;
 
 /**
- * This interface defines the properties and methods for a Unit in the game.
+ * Abstract class representing a Unit in the game.
  * 
  * @author Anton Valkenberg (anton.valkenberg@gmail.com)
  *
  */
-public interface Unit {
+@Setter
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+public abstract class Unit extends GameObject {
 
-	public int position;
+	//region Constants
 	
-	public Direction orientation;
+	/**
+	 * Default health points for a Unit.
+	 */
+	public static final int DEFAULT_UNIT_HP = 5;
 	
-	public int fieldOfViewRange;
+	/**
+	 * Default orientation for a Unit.
+	 */
+	public static final Direction DEFAULT_ORIENTATION = Direction.NORTH;
 	
-	public int fieldOfViewAngle;
+	/**
+	 * Default Field of View range for a Unit.
+	 */
+	public static final int DEFAULT_FOV_RANGE = 3;
 	
-	public int attackRange;
+	/**
+	 * Default Field of View angle for a Unit.
+	 */
+	public static final int DEFAULT_FOV_ANGLE = 90;
 	
-	public int attackDamage;
+	/**
+	 * Default attack range for a Unit.
+	 */
+	public static final int DEFAULT_ATTACK_RANGE = 4;
 	
-	public int specialAttackCooldown;
+	/**
+	 * Default attack damage for a Unit.
+	 */
+	public static final int DEFAULT_ATTACK_DAMAGE = 3;
 	
-	public int spawnCost;
+	/**
+	 * Default cooldown for a Unit's special attack.
+	 */
+	public static final int DEFAULT_SPECIAL_COOLDOWN = 5;
 	
-	public int scoreWorth;
+	/**
+	 * Default spawn cost for a Unit.
+	 */
+	public static final int DEFAULT_SPAWN_COST = 5;
+	
+	/**
+	 * Default score that a Unit is worth.
+	 */
+	public static final int DEFAULT_SCORE = 25;
+
+	//endregion
+	
+	//region Properties
+
+	/**
+	 * The Direction the Unit is facing.
+	 */
+	public Direction orientation = DEFAULT_ORIENTATION;
+	
+	/**
+	 * The range (in squares) of the Unit's Field of View.
+	 */
+	public int fieldOfViewRange = DEFAULT_FOV_RANGE;
+	
+	/**
+	 * The angle (in degrees) of the Unit's Field of View.
+	 */
+	public int fieldOfViewAngle = DEFAULT_FOV_ANGLE;
+	
+	/**
+	 * The range (in squares) of the Unit's attacks.
+	 */
+	public int attackRange = DEFAULT_ATTACK_RANGE;
+	
+	/**
+	 * The damage the Unit's attacks inflict.
+	 */
+	public int attackDamage = DEFAULT_ATTACK_DAMAGE;
+	
+	/**
+	 * The remaining cool down time (in ticks) of the Unit's special attack.
+	 */
+	public int specialAttackCooldown = DEFAULT_SPECIAL_COOLDOWN;
+	
+	/**
+	 * The cost to spawn an instance of this Unit.
+	 */
+	public int spawnCost = DEFAULT_SPAWN_COST;
+	
+	/**
+	 * The score this Unit is worth when defeated by the opposing team.
+	 */
+	public int scoreWorth = DEFAULT_SCORE;
+
+	//endregion
+	
+	//region Constructor
+
+	/**
+	 * Constructs a new instance of a Unit with default HP.
+	 * @param mapPosition The Unit's position on the Map.
+	 * @param facing The Direction the Unit is facing.
+	 * @param fovRange The Unit's Field of View range.
+	 * @param fovAngle The Unit's Field of View angle.
+	 * @param attckRange The Unit's attack range.
+	 * @param attckDmg The Unit's attack damage.
+	 * @param cooldown The cooldown of the Unit's special attack.
+	 * @param cost The cost to spawn the Unit.
+	 * @param score The score the Unit is worth.
+	 */
+	public Unit(int mapPosition, Direction facing, int fovRange, int fovAngle, int attckRange, int attckDmg, int cooldown, int cost, int score) {
+		this(mapPosition, DEFAULT_UNIT_HP, facing, fovRange, fovAngle, attckRange, attckDmg, cooldown, cost, score);
+	}
+	
+	/**
+	 * Constructs a new instance of a Unit.
+	 * @param mapPosition The Unit's position on the Map.
+	 * @param maxHP The Unit's maximum number of health points.
+	 * @param facing The Direction the Unit is facing.
+	 * @param fovRange The Unit's Field of View range.
+	 * @param fovAngle The Unit's Field of View angle.
+	 * @param attckRange The Unit's attack range.
+	 * @param attckDmg The Unit's attack damage.
+	 * @param cooldown The cooldown of the Unit's special attack.
+	 * @param cost The cost to spawn the Unit.
+	 * @param score The score the Unit is worth.
+	 */
+	public Unit(int mapPosition, int maxHP, Direction facing, int fovRange, int fovAngle, int attckRange, int attckDmg, int cooldown, int cost, int score) {
+		super(mapPosition, maxHP);
+		orientation = facing;
+		fieldOfViewRange = fovRange;
+		fieldOfViewAngle = fovAngle;
+		attackRange = attckRange;
+		attackDamage = attckDmg;
+		specialAttackCooldown = cooldown;
+		spawnCost = cost;
+		scoreWorth = score;
+	}
+	
+	//endregion
+	
 }
