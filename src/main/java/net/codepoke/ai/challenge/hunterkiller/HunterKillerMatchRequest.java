93172812e57c8ff676a15b6e6d3f91f570e469f3
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2017-03-07 21:59:46 +0100

    Added overrides for methods 'isCompatible' and 'mergeRequest'

--- src/main/java/net/codepoke/ai/challenge/hunterkiller/HunterKillerMatchRequest.java
+++ src/main/java/net/codepoke/ai/challenge/hunterkiller/HunterKillerMatchRequest.java
@@ -1,10 +1,13 @@
 package net.codepoke.ai.challenge.hunterkiller;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import net.codepoke.ai.challenge.hunterkiller.enums.GameMode;
 import net.codepoke.ai.challenge.hunterkiller.enums.MapType;
 import net.codepoke.ai.network.MatchRequest;
+
+import com.badlogic.gdx.utils.Array;
 
 /**
  * Create a request for a match of HunterKiller with specific parameters
@@ -12,8 +15,9 @@ import net.codepoke.ai.network.MatchRequest;
  * @author Anton Valkenberg (anton.valkenberg@gmail.com)
  *
  */
+@Data
 @NoArgsConstructor
-@Setter
+@EqualsAndHashCode(callSuper = true)
 public class HunterKillerMatchRequest
 		extends MatchRequest {
 
@@ -49,4 +53,75 @@ public class HunterKillerMatchRequest
 		super(HunterKillerConstants.GAME_NAME, botUID, training);
 	}
 
+	/**
+	 * Another MatchRequest is compatible with this HunterKillerMatchRequest if the other request is a
+	 * {@link HunterKillerMatchRequest}, they are compatible {@link MatchRequest}s, and all of the following statements
+	 * are true:
+	 * <ul>
+	 * <li>Both requests specify the same MapType, if any.</li>
+	 * <li>Both requests specify the same GameMode, if any.</li>
+	 * <li>Both requests specify the same map name, if any.</li>
+	 * <li>Both requests specify the same options, if any.</li>
+	 * </ul>
+	 * 
+	 * If the other MatchRequest is not a {@link HunterKillerMatchRequest}, compatibility is decided by
+	 * {@link MatchRequest#isCompatible(MatchRequest, String, Array)}.
+	 */
+	@Override
+	public boolean isCompatible(MatchRequest other, String otherBotName, Array<String> alreadyConnectedPlayers) {
+		// Check if the requests are compatible on a MatchRequest level
+		boolean superCompatible = super.isCompatible(other, otherBotName, alreadyConnectedPlayers);
+
+		// Check if the other request is not a HunterKillerMatchRequest
+		if (!(other instanceof HunterKillerMatchRequest)) {
+			return superCompatible;
+		}
+
+		// It's a HKMR, so check if our special settings are the same
+		HunterKillerMatchRequest otherHKMR = (HunterKillerMatchRequest) other;
+
+		if (mapType != null && mapType != otherHKMR.mapType) {
+			return false;
+		}
+
+		if (gameType != null && gameType != otherHKMR.gameType) {
+			return false;
+		}
+
+		if (mapName != null && !mapName.equals(otherHKMR.mapName)) {
+			return false;
+		}
+
+		if (options != null && !options.equals(otherHKMR.options)) {
+			return false;
+		}
+
+		// None of the check for HunterKiller specific settings failed, so return the base compatibility
+		return superCompatible;
+	}
+
+	@Override
+	public void mergeRequest(String myBotName, MatchRequest other) {
+		super.mergeRequest(myBotName, other);
+
+		// Check if the other request is a HunterKillerMatchRequest, if not then we are done
+		if (!(other instanceof HunterKillerMatchRequest))
+			return;
+
+		// It's a HKMR, so check if any options from the other request are more restrictive
+		HunterKillerMatchRequest otherHKMR = (HunterKillerMatchRequest) other;
+
+		if (mapType == null && otherHKMR.mapType != null) {
+			mapType = otherHKMR.mapType;
+		}
+		if (gameType == null && otherHKMR.gameType != null) {
+			gameType = otherHKMR.gameType;
+		}
+		if (mapName == null && otherHKMR.mapName != null) {
+			mapName = otherHKMR.mapName;
+		}
+		if (options == null && otherHKMR.options != null) {
+			options = otherHKMR.options;
+		}
+	}
 }
