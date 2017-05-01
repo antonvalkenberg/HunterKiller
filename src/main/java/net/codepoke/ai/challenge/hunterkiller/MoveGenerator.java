880d18e4697606b946939ec8ca4497f6113bf000
antonvalkenberguni <a.valkenberg@student.maastrichtuniversity.nl>
2017-05-01 18:43:51 +0200

    Fixed some things in generation of random orders.

--- src/main/java/net/codepoke/ai/challenge/hunterkiller/MoveGenerator.java
+++ src/main/java/net/codepoke/ai/challenge/hunterkiller/MoveGenerator.java
@@ -5,8 +5,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
-import com.badlogic.gdx.utils.Array;
-
 import net.codepoke.ai.challenge.hunterkiller.enums.Direction;
 import net.codepoke.ai.challenge.hunterkiller.enums.StructureOrderType;
 import net.codepoke.ai.challenge.hunterkiller.enums.UnitOrderType;
@@ -23,6 +21,8 @@ import net.codepoke.ai.challenge.hunterkiller.orders.HunterKillerOrder;
 import net.codepoke.ai.challenge.hunterkiller.orders.StructureOrder;
 import net.codepoke.ai.challenge.hunterkiller.orders.UnitOrder;
 
+import com.badlogic.gdx.utils.Array;
+
 /**
  * This class generates legal {@link HunterKillerOrder}s.
  * 
@@ -30,19 +30,33 @@ import net.codepoke.ai.challenge.hunterkiller.orders.UnitOrder;
  *
  */
 public class MoveGenerator {
-	
+
 	/**
 	 * Simple Random to avoid alloc'ing
 	 */
 	public static Random RNG = new Random();
-	
+
 	/**
-	 * Temporary arrays used to ad-hoc shuffle to create RNG.
-	 * TODO: Move this to instance class because of MOOOEELLTTIEEE THREADING
+	 * Arrays used to ad-hoc shuffle to create RNG.
 	 */
-	private static Array<UnitType> UNIT_TYPES = Array.with(UnitType.values);
-	private static Array<Direction> DIRECTIONS = Array.with(Direction.values);
-	private static Array<UnitOrderType> UNIT_ORDER_TYPES = Array.with(UnitOrderType.values);
+	private static ThreadLocal<Array<UnitType>> UNIT_TYPES = new ThreadLocal<Array<UnitType>>() {
+		@Override
+		protected Array<UnitType> initialValue() {
+			return Array.with(UnitType.values);
+		}
+	};
+	private static ThreadLocal<Array<Direction>> DIRECTIONS = new ThreadLocal<Array<Direction>>() {
+		@Override
+		protected Array<Direction> initialValue() {
+			return Array.with(Direction.values);
+		}
+	};
+	private static ThreadLocal<Array<UnitOrderType>> UNIT_ORDER_TYPES = new ThreadLocal<Array<UnitOrderType>>() {
+		@Override
+		protected Array<UnitOrderType> initialValue() {
+			return Array.with(UnitOrderType.values);
+		}
+	};
 
 	/**
 	 * Returns a list containing all legal orders for a structure in the current state. For a list of all types of
@@ -72,7 +86,7 @@ public class MoveGenerator {
 		// Return the list of legal orders
 		return orders;
 	}
-	
+
 	/**
 	 * Returns a list containing all legal orders for a structure in the current state. For a list of all types of
 	 * orders available to a Structure, see {@link StructureOrderType}.
@@ -84,10 +98,11 @@ public class MoveGenerator {
 	 */
 	public static StructureOrder getRandomOrder(HunterKillerState state, Structure structure) {
 
-		UNIT_TYPES.shuffle();
-		
+		Array<UnitType> types = UNIT_TYPES.get();
+		types.shuffle();
+
 		// Check for each unit type if the structure can spawn it
-		for (UnitType type : UNIT_TYPES) {
+		for (UnitType type : types) {
 			if (structure.canSpawn(state, type))
 				return structure.spawn(type);
 		}
@@ -95,22 +110,23 @@ public class MoveGenerator {
 		// Return the list of legal orders
 		return null;
 	}
-	
+
 	/**
 	 * Returns a random order for the given unit.
 	 */
-	public static UnitOrder getRandomOrder(HunterKillerState state, Unit unit){
-						
-		UNIT_ORDER_TYPES.shuffle();
-		
-		for (UnitOrderType type : UNIT_ORDER_TYPES) {
-			
+	public static UnitOrder getRandomOrder(HunterKillerState state, Unit unit) {
+
+		Array<UnitOrderType> types = UNIT_ORDER_TYPES.get();
+		types.shuffle();
+
+		for (UnitOrderType type : types) {
+
 			UnitOrder move = null;
-			
-			if(!type.hasLocation){				
+
+			if (!type.hasLocation) {
 				// Rotate
-				return unit.rotate(type == UnitOrderType.ROTATE_CLOCKWISE);				
-			} else if(type != UnitOrderType.MOVE){				
+				return unit.rotate(type == UnitOrderType.ROTATE_CLOCKWISE);
+			} else if (type != UnitOrderType.MOVE) {
 				// Attack
 				move = getRandomAttackOrder(state, unit, false, type == UnitOrderType.ATTACK_SPECIAL);
 			} else {
@@ -118,9 +134,10 @@ public class MoveGenerator {
 				move = getRandomMoveOrder(state, unit);
 			}
 
-			if(move != null) return move;				
+			if (move != null)
+				return move;
 		}
-		
+
 		return null;
 	}
 
@@ -195,25 +212,25 @@ public class MoveGenerator {
 		return orders;
 	}
 
-
 	/**
-	 * Returns a random legal move order. See {@link UnitOrderType} for a list of possible orders. 
+	 * Returns a random legal move order. See {@link UnitOrderType} for a list of possible orders.
 	 */
 	public static UnitOrder getRandomMoveOrder(HunterKillerState state, Unit unit) {
 
 		// Get the map we are currently on & the unit's location
 		Map map = state.getMap();
 		MapLocation unitLocation = unit.getLocation();
-		
-		DIRECTIONS.shuffle();
-		
+
+		Array<Direction> directions = DIRECTIONS.get();
+		directions.shuffle();
+
 		// Check what movement options we have
-		for (Direction direction : DIRECTIONS) {
+		for (Direction direction : directions) {
 			MapLocation newLocaction = map.getAdjacentLocationInDirection(unitLocation, direction);
 			if (map.isMovePossible(unitLocation, newLocaction))
 				return unit.move(newLocaction, map);
 		}
-		
+
 		return null;
 	}
 
@@ -310,10 +327,10 @@ public class MoveGenerator {
 		// Return the list of legal orders
 		return orders;
 	}
-	
 
 	/**
-	 * Returns a random legal attack order, from the Unit's Field-of-View. See {@link UnitOrderType} for a list of possible orders.
+	 * Returns a random legal attack order, from the Unit's Field-of-View. See {@link UnitOrderType} for a list of
+	 * possible orders.
 	 * Friendly-fire moves are already removed.
 	 * 
 	 * @param state
@@ -324,10 +341,11 @@ public class MoveGenerator {
 	 *            Whether or not to use the Player's Field-of-View, instead of the Unit's.
 	 */
 	public static UnitOrder getRandomAttackOrder(HunterKillerState state, Unit unit, boolean usePlayersFoV, boolean useSpecial) {
-		
+
 		// If we want special, but we can't, stop
-		if(useSpecial && unit.getSpecialAttackCooldown() > 0) return null;
-		
+		if (useSpecial && unit.getSpecialAttackCooldown() > 0)
+			return null;
+
 		// Get the map we are currently on
 		Map map = state.getMap();
 		// And the unit's location
@@ -345,84 +363,100 @@ public class MoveGenerator {
 		}
 
 		if (unit instanceof Infected) {
-			
-			DIRECTIONS.shuffle();
-			
+
+			Array<Direction> directions = DIRECTIONS.get();
+			directions.shuffle();
+
 			// Since we know an infected can only do a melee attack (range = 1)
-			for (Direction direction : DIRECTIONS) {
+			for (Direction direction : directions) {
 				// Make an order for the first adjacent location that contains a unit.
 				MapLocation targetLocation = map.getAdjacentLocationInDirection(unitLocation, direction);
-				
-				if(targetLocation == null) continue;
-				
-				Unit target = map.getUnitAtLocation(targetLocation);				
-				if(target != null){
-					
+
+				if (targetLocation == null)
+					continue;
+
+				Unit target = map.getUnitAtLocation(targetLocation);
+				if (target != null) {
+
 					// No sense in killing allied infected.
-					if(target instanceof Infected && target.getControllingPlayerID() == unit.getControllingPlayerID()) continue;
-					
+					if (target instanceof Infected && target.getControllingPlayerID() == unit.getControllingPlayerID())
+						continue;
+
 					return unit.attack(targetLocation, false);
-				} else {					
+				} else {
 					MapFeature feature = map.getFeatureAtLocation(targetLocation);
-					if(!(feature instanceof Structure)) continue;
-					if(((Structure)feature).getControllingPlayerID() == unit.getControllingPlayerID()) continue;
+					if (!(feature instanceof Structure))
+						continue;
+					if (((Structure) feature).getControllingPlayerID() == unit.getControllingPlayerID())
+						continue;
 
 					return unit.attack(targetLocation, false);
 				}
 			}
-			
+
 		} else {
 			// Get the unit's attack range
 			int attackRange = Unit.getAttackRange(unit.getType());
-			boolean specialAvailable = unit.getSpecialAttackCooldown() <= 0;
-			
+
 			// TODO: Optimize
 			Array<MapLocation> locations = new Array<MapLocation>();
 
 			for (MapLocation location : fov) {
 				locations.add(location);
 			}
-			
+
 			locations.shuffle();
 
 			// Go through the field-of-view
 			for (MapLocation location : locations) {
-				
+
 				// Check if this location is within the unit's attack range
-				if (map.getDistance(unitLocation, location) > attackRange) continue;					
+				if (map.getDistance(unitLocation, location) > attackRange)
+					continue;
 
 				Controlled target = map.getUnitAtLocation(location);
 				MapFeature feature = null;
-				
-				if(target == null){
+
+				if (target == null) {
 					feature = map.getFeatureAtLocation(location);
-					if(feature instanceof Controlled)
-						target = (Controlled) feature; 
+					if (feature instanceof Controlled)
+						target = (Controlled) feature;
 				}
-				
-				// If we don't target anything, continue; If we target friendlies, continue.
-				if(target == null && feature == null) continue;				
-				
-				if(useSpecial){
-					
+
+				// If we don't target anything, continue
+				if (target == null)
+					continue;
+				// If we target a friendly feature, continue
+				if (target instanceof MapFeature && target.getControllingPlayerID() == unit.getControllingPlayerID())
+					continue;
+
+				if (useSpecial) {
+
 					// Soldiers can't grenade walls
 					if (unit instanceof Soldier && feature instanceof Wall)
 						continue;
-					
+
 					// Do not target friendlies if we aren't a medic.
-					if(target != null 
-							&& !(unit instanceof Medic) 
-							&& target.getControllingPlayerID() == unit.getControllingPlayerID())
+					if (!(unit instanceof Medic) && target.getControllingPlayerID() == unit.getControllingPlayerID())
 						continue;
-										
+
+					// Do not heal enemies
+					if (unit instanceof Medic && target.getControllingPlayerID() != unit.getControllingPlayerID())
+						continue;
+
+					// Do not heal non-damaged units
+					if (unit instanceof Medic && target instanceof Unit && !((Unit) target).isDamaged())
+						continue;
+
 					return unit.attack(location, true);
 				} else {
-					
+
 					// Don't target friendlies
-					if(target != null && target.getControllingPlayerID() == unit.getControllingPlayerID()) continue;
-					
-					return unit.attack(location, false);					
-				}				
+					if (target.getControllingPlayerID() == unit.getControllingPlayerID())
+						continue;
+
+					return unit.attack(location, false);
+				}
 			}
 		}
 
